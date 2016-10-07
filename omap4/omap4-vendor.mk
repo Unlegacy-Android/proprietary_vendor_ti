66e6145b917b2de0168b4b673c7079716541d0ba
D. Andrei Măceș <dmaces@nd.edu>
2016-10-07 15:21:21 -0400

    omap4: Promote VENDOR_SGX_CORE for reuse in hardware
    Change-Id: Ib6a6bbd2ba44c81bdc79c82acbe28832fc516453

--- omap4/omap4-vendor.mk
+++ omap4/omap4-vendor.mk
@@ -1,5 +1,11 @@
 $(call inherit-product, vendor/ti/omap4/omap4-vendor-blobs.mk)
 
+ifeq ($(TARGET_BOARD_OMAP_CPU),4470)
+VENDOR_SGX_CORE := SGX544_112
+else
+VENDOR_SGX_CORE := SGX540_120
+endif
+
 PRODUCT_PACKAGES += \
 	libglslcompiler \
 	libIMGegl \
